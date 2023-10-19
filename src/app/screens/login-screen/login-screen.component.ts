b195cea411cc82d89b67ccc9fee9d5d661e4b496
IgniferDev <ferbena2213@gmail.com>
2023-10-19 17:09:58 -0600

    Segun back end
    19 10 23 ya por fin el error era el python tenia 3.12 y solo es compatible hasta 3.9 ya quedo por fin subo esto como copia de emergencia respaldo hasta el registro y todo eso buenas

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -14,6 +14,11 @@ export class LoginScreenComponent implements OnInit {
   public password: String = "";
 
   public errors:any = {};
+
+  public users_registrados: any = [];
+  public logeo: boolean = false;
+  public flag_pwd: boolean = false;
+  public flag_email: boolean = false;
   
   constructor(
     private router: Router,
@@ -21,6 +26,43 @@ export class LoginScreenComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.llenadoUsuarios();
+  }
+
+  public llenadoUsuarios() {
+    this.users_registrados = [
+      {
+      'matricula': '202234180',
+      'first_name': 'Fernando',
+      'last_name': 'Morales',
+      'email': 'a@gmail.com',
+      'password': 'rock2213',
+      'confirmar_password': 'rock2213',
+      'fecha_nacimiento': '2004-01-01',
+      'curp': 'MESL951007HVZNNS01',
+      'rfc': 'MESL951007S73',
+      'edad': '17',
+      'telefono': '2225220150',
+      'ocupacion': 'Estudiante',
+      },
+      {
+        'matricula': '123456789',
+      'first_name': 'Alfredo',
+      'last_name': 'Herrera Mora',
+      'email': 'alfredo@gmail.com',
+      'password': 'rock2213',
+      'confirmar_password': 'rock2213',
+      'fecha_nacimiento': '2000-01-01',
+      'curp': 'MESL951007HVZNNS01',
+      'rfc': 'MESL951007S73',
+      'edad': '20',
+      'telefono': '2225220150',
+      'ocupacion': 'Administrador',
+      }
+    ];
+    console.log("Usuario es: ", this.users_registrados);
+    
+    
   }
 
   public login(){
@@ -31,9 +73,41 @@ export class LoginScreenComponent implements OnInit {
     if(!$.isEmptyObject(this.errors)){
       return false;
     }
+    console.log("paso validacion");
+    
     //Si pasa la validación
-    this.router.navigate(["home"]);
+    
+    this.buscarUser(this.username, this.password);
+
+    if(this.logeo){
+      //alert("Usuario Encontrado");
+      this.router.navigate(["home"]);
+    }else{
+      alert("Usuario y contraseña incorrectos");
+    }
+
   }
+
+  public buscarUser(username: String, pwd: String){
+    //Tendria que logearse
+
+    this.users_registrados.forEach(user => {
+      if(user.email == username){
+        if(user.password== pwd){
+          this.logeo=true;
+        }
+        else{
+          this.flag_pwd=true;
+          //Si esta mal la contraseña
+        }
+      }
+      else{
+        this.flag_email=true;
+        //Si esta mal el correo
+      }
+    });
+  }
+
 
   public showPassword(){
     if(this.type == "password"){
